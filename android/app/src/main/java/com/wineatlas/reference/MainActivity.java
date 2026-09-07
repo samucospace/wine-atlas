a5a388dd45793c0a2669523027b39c7cff93960c
﻿Sam Fraser <samfraser.au@gmail.com>
2026-09-07 13:30:30 +0800

    Build android app

---
+++ android/app/src/main/java/com/wineatlas/reference/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wineatlas.reference;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: "com.wineatlas.reference",
   appName: "Wine Atlas",
   webDir: "out",
+  backgroundColor: "#24372e",
+  android: {
+    appendUserAgent: " WineAtlasAndroid/1.0",
+  },
 };
 
 export default config;
